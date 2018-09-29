fa50b2da6c8edfeb6dfc74ec6e7e4e5f18711ed8
Kotovа Katerina <vseprint@gmail>
2018-09-29 16:26:52 +0300

    commit 3 add version and name, added print-numbers.py

--- print-numbers.py
+++ print-numbers.py
@@ -1,5 +1,15 @@
 import sys
-print(sys.argv)
+# 't' means test, t-0.1 test version 0.1
+VERSION = "t-0.1"
+PROGRAM_NAME = sys.argv[0]
+
+if "-h" in sys.argv:
+    print("Usage: {} [OPTION]... [NUMBER]".format(PROGRAM_NAME))
+    print("-h   display this help and exit")
+    print("-v   output version information and exit")
+
+if "-v" in sys.argv:
+    print("{} version {}".format(PROGRAM_NAME, VERSION))
 
 # count = 10
 # for i in range(count):
