4f238656426ddf5a29b1d2020a5a7a7b8c42fe3f
Kotovа Katerina <vseprint@gmail>
2018-09-30 15:36:31 +0300

    commit 5 version 1.0, added print-numbers.py

--- print-numbers.py
+++ print-numbers.py
@@ -1,8 +1,8 @@
 import sys
 # 't' means test, t-0.1 test version 0.1
 
-VERSION = "t-0.2"
-# sys.argv - list of arguments, example ['print-numbers.py', 'arg1', 'arg2']
+VERSION = "1.0"
+# sys.argv - list of arguments, example ['print-numbers.py', 'arg1', 'arg2', ...]
 # sys.argv[0] - is the name of the python file
 PROGRAM_NAME = sys.argv[0]
 
@@ -19,4 +19,4 @@ if "-v" in sys.argv:
 # sys.argv[1] is string type, we need to convert to integer type using int() function
 count = int(sys.argv[1])
 for i in range(count):
-    print(i)
+    print(i)  
